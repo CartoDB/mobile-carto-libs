60faf7718c8cd69cf9a86b0e2ede8ecff48d4070
mtehver <mark.tehver@gmail.com>
2019-04-02 23:22:36 +0300

    Explicit initialization of all members in ViewState

--- vt/src/vt/ViewState.h
+++ vt/src/vt/ViewState.h
@@ -27,8 +27,8 @@ namespace carto { namespace vt {
         cglib::frustum3<double> frustum;
         std::array<cglib::vec3<float>, 3> orientation;
 
-        ViewState() : zoom(0), aspect(1), resolution(1), zoomScale(1), projectionMatrix(cglib::mat4x4<double>::identity()), origin(0, 0, 0), frustum(), orientation() { }
-        explicit ViewState(const cglib::mat4x4<double>& projectionMatrix, const cglib::mat4x4<double>& cameraMatrix, float zoom, float aspect, float resolution) : zoom(zoom), aspect(aspect), resolution(resolution), zoomScale(std::pow(2.0f, -zoom)), projectionMatrix(projectionMatrix), cameraMatrix(cameraMatrix) {
+        ViewState() : zoom(0), aspect(1), resolution(1), zoomScale(1), projectionMatrix(cglib::mat4x4<double>::identity()), cameraMatrix(cglib::mat4x4<double>::identity()), origin(0, 0, 0), frustum(), orientation() { }
+        explicit ViewState(const cglib::mat4x4<double>& projectionMatrix, const cglib::mat4x4<double>& cameraMatrix, float zoom, float aspect, float resolution) : zoom(zoom), aspect(aspect), resolution(resolution), zoomScale(std::pow(2.0f, -zoom)), projectionMatrix(projectionMatrix), cameraMatrix(cameraMatrix), origin(), frustum(), orientation() {
             cglib::mat4x4<double> invCameraMatrix = cglib::inverse(cameraMatrix);
             origin = cglib::proj_p(cglib::col_vector(invCameraMatrix, 3));
             frustum = cglib::gl_projection_frustum(projectionMatrix * cameraMatrix);
