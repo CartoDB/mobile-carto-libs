eb239ff4572efda63b7dada388bca187fb06bd87
mtehver <mark.tehver@gmail.com>
2022-06-20 10:44:46 +0300

    Fix previous commit

--- mapnikvt/src/mapnikvt/SymbolizerContext.cpp
+++ mapnikvt/src/mapnikvt/SymbolizerContext.cpp
@@ -14,19 +14,21 @@ namespace carto::mvt {
     SymbolizerContext::Settings::Settings(float tileSize, std::shared_ptr<const std::map<std::string, Value>> nutiParameterValueMap, std::shared_ptr<const vt::Font> fallbackFont) :
         _tileSize(tileSize), _geometryScale(1.0f), _fontScale(1.0f), _zoomLevelBias(0.0f), _nutiParameterValueMap(std::move(nutiParameterValueMap)), _fallbackFont(std::move(fallbackFont))
     {
-        auto geometryScaleIt = _nutiParameterValueMap.find("_geometryscale");
-        if (geometryScaleIt != _nutiParameterValueMap.end()) {
-            _geometryScale = ValueConverter<float>::convert(geometryScaleIt->second);
-        }
+        if (_nutiParameterValueMap) {
+            auto geometryScaleIt = _nutiParameterValueMap->find("_geometryscale");
+            if (geometryScaleIt != _nutiParameterValueMap->end()) {
+                _geometryScale = ValueConverter<float>::convert(geometryScaleIt->second);
+            }
 
-        auto fontScaleIt = _nutiParameterValueMap.find("_fontscale");
-        if (fontScaleIt != _nutiParameterValueMap.end()) {
-            _fontScale = ValueConverter<float>::convert(fontScaleIt->second);
-        }
-        
-        auto zoomLevelBiasIt = _nutiParameterValueMap.find("_zoomlevelbias");
-        if (zoomLevelBiasIt != _nutiParameterValueMap.end()) {
-            _zoomLevelBias = ValueConverter<float>::convert(zoomLevelBiasIt->second);
+            auto fontScaleIt = _nutiParameterValueMap->find("_fontscale");
+            if (fontScaleIt != _nutiParameterValueMap->end()) {
+                _fontScale = ValueConverter<float>::convert(fontScaleIt->second);
+            }
+            
+            auto zoomLevelBiasIt = _nutiParameterValueMap->find("_zoomlevelbias");
+            if (zoomLevelBiasIt != _nutiParameterValueMap->end()) {
+                _zoomLevelBias = ValueConverter<float>::convert(zoomLevelBiasIt->second);
+            }
         }
     }
 }
